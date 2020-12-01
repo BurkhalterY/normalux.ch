d4642d53131a66c0b877e2bc15d2602c0d19f988
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2020-12-01 19:31:43 +0100

    Better contact page

--- application/views/misc/contact.php
+++ application/views/misc/contact.php
@@ -21,36 +21,21 @@
 		<br><br>
 		<h2><?=$this->lang->line('our_other_projects')?></h2>
 		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="https://chat.normalux.ch" target="_blank"><img src="<?=base_url('medias/divers/thumbails/chat.png')?>" alt="chat.normalux.ch"></a>
-			<div class="desc">chat.normalux.ch</div>
+			<a href="https://github.com/BurkhalterY/EPSIC_Bataille_Navale/releases" target="_blank"><img src="<?=base_url('assets/images/thumbails/battleship.png')?>" alt="Bataille Navale C#"></a>
+			<div class="desc">Bataille Navale C#</div>
 		</div>
+		<h3>Collection de BD :</h2>
 		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="<?=base_url('medias/divers/TRUMP - L\'intégrale/TRUMP - La revanche de l_Univers.pdf')?>" target="_blank"><img src="<?=base_url('medias/divers/thumbails/TRUMP - La revanche de l_Univers.png')?>" alt="TRUMP - La revanche de l'Univers"></a>
+			<a href="<?=base_url('medias/divers/TRUMP - L\'intégrale/TRUMP - La revanche de l_Univers.pdf')?>" target="_blank"><img src="<?=base_url('assets/images/thumbails/TRUMP - La revanche de l_Univers.png')?>" alt="TRUMP - La revanche de l'Univers"></a>
 			<div class="desc">TRUMP - La revanche de l'Univers</div>
 		</div>
 		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="<?=base_url('medias/divers/TRUMP - L\'intégrale/TRUMP - Tome2.pdf')?>" target="_blank"><img src="<?=base_url('medias/divers/thumbails/TRUMP - Tome2.png')?>" alt="TRUMP - Tome 2"></a>
+			<a href="<?=base_url('medias/divers/TRUMP - L\'intégrale/TRUMP - Tome2.pdf')?>" target="_blank"><img src="<?=base_url('assets/images/thumbails/TRUMP - Tome2.png')?>" alt="TRUMP - Tome 2"></a>
 			<div class="desc">TRUMP - Tome 2</div>
 		</div>
 		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="<?=base_url('medias/divers/TRUMP - L\'intégrale/Tome 3 - Spécial anime !.pdf')?>" target="_blank"><img src="<?=base_url('medias/divers/thumbails/Tome 3 - Spécial anime !.jpg')?>" alt="TRUMP - Tome3 : Spécial anime !"></a>
+			<a href="<?=base_url('medias/divers/TRUMP - L\'intégrale/Tome 3 - Spécial anime !.pdf')?>" target="_blank"><img src="<?=base_url('assets/images/thumbails/Tome 3 - Spécial anime !.jpg')?>" alt="TRUMP - Tome3 : Spécial anime !"></a>
 			<div class="desc">TRUMP - Tome3 : Spécial anime !</div>
-		</div>
-		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="https://github.com/BurkhalterY/EPSIC_Bataille_Navale/releases" target="_blank"><img src="<?=base_url('medias/divers/thumbails/battleship.png')?>" alt="Bataille Navale C#"></a>
-			<div class="desc">Bataille Navale C#</div>
-		</div>
-		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="https://github.com/BurkhalterY/UseYourWords/releases" target="_blank"><img src="<?=base_url('medias/divers/thumbails/UseYourWords.jpeg')?>" alt="Use Your Words Cheater"></a>
-			<div class="desc">Use Your Words Cheater</div>
-		</div>
-		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="https://wp.normalux.ch" target="_blank"><img src="<?=base_url('medias/divers/thumbails/wp.png')?>" alt="Mon blog WordPress"></a>
-			<div class="desc">Mon blog WordPress</div>
-		</div>
-		<div class="gallery" style="float: none; display: inline-block;">
-			<a href="https://japonais.normalux.ch" target="_blank"><img src="<?=base_url('medias/divers/thumbails/japonais.png')?>" alt="Site de révision du Japonais"></a>
-			<div class="desc">Site de révision du Japonais</div>
 		</div>
 	</div>
 </article>
