7b868e739cdba11b28bd4649eb07cb8ef264275c
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2020-12-01 19:16:54 +0100

    Update db

--- database/normalux.sql
+++ database/normalux.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : ven. 04 sep. 2020 à 14:23
+-- Généré le : mar. 01 déc. 2020 à 19:15
 -- Version du serveur :  10.2.32-MariaDB
--- Version de PHP : 7.3.18
+-- Version de PHP : 7.3.22
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -72,24 +72,6 @@ CREATE TABLE `drawings` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `kdlt`
---
-
-CREATE TABLE `kdlt` (
-  `id` int(11) NOT NULL,
-  `kanji` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `keyword` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `memo` text COLLATE utf8_unicode_ci NOT NULL,
-  `lines_number` int(11) NOT NULL,
-  `no_kdlt` int(11) DEFAULT NULL,
-  `chapter` int(11) NOT NULL,
-  `remarque` text COLLATE utf8_unicode_ci NOT NULL,
-  `composant` text COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `pictures`
 --
 
@@ -113,36 +95,6 @@ CREATE TABLE `suggestions` (
   `content` text COLLATE utf8_unicode_ci NOT NULL,
   `date_post` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `tracings`
---
-
-CREATE TABLE `tracings` (
-  `id` int(11) NOT NULL,
-  `fk_word` int(11) NOT NULL,
-  `filename` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `json` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `correct` tinyint(1) DEFAULT NULL,
-  `ip` varchar(45) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tracing_date` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `units`
---
-
-CREATE TABLE `units` (
-  `id` int(11) NOT NULL,
-  `fk_user` int(11) DEFAULT NULL,
-  `no` float NOT NULL,
-  `unit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `archive` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -173,34 +125,15 @@ CREATE TABLE `users_types` (
   `level` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `votes`
+-- Déchargement des données de la table `users_types`
 --
 
-CREATE TABLE `votes` (
-  `id` int(11) NOT NULL,
-  `fk_user` int(11) NOT NULL,
-  `fk_drawing` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `words`
---
-
-CREATE TABLE `words` (
-  `id` int(11) NOT NULL,
-  `french` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kana` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `kanji` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `fk_unit` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT 1,
-  `nb_time` int(11) NOT NULL DEFAULT 0,
-  `archive` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `users_types` (`id`, `grade`, `level`) VALUES
+(1, 'Visiteur', 1),
+(2, 'Utilisateur', 2),
+(3, 'Modérateur', 4),
+(4, 'Super Admin', 8);
 
 --
 -- Index pour les tables déchargées
@@ -229,12 +162,6 @@ ALTER TABLE `drawings`
   ADD KEY `fk_picture` (`fk_picture`);
 
 --
--- Index pour la table `kdlt`
---
-ALTER TABLE `kdlt`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Index pour la table `pictures`
 --
 ALTER TABLE `pictures`
@@ -244,20 +171,6 @@ ALTER TABLE `pictures`
 -- Index pour la table `suggestions`
 --
 ALTER TABLE `suggestions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user` (`fk_user`);
-
---
--- Index pour la table `tracings`
---
-ALTER TABLE `tracings`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_word` (`fk_word`);
-
---
--- Index pour la table `units`
---
-ALTER TABLE `units`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user` (`fk_user`);
 
@@ -276,21 +189,6 @@ ALTER TABLE `users_types`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `votes`
---
-ALTER TABLE `votes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_user` (`fk_user`),
-  ADD KEY `fk_drawing` (`fk_drawing`);
-
---
--- Index pour la table `words`
---
-ALTER TABLE `words`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_unit` (`fk_unit`);
-
---
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
@@ -307,12 +205,6 @@ ALTER TABLE `drawings`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `kdlt`
---
-ALTER TABLE `kdlt`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT pour la table `pictures`
 --
 ALTER TABLE `pictures`
@@ -325,18 +217,6 @@ ALTER TABLE `suggestions`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `tracings`
---
-ALTER TABLE `tracings`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `units`
---
-ALTER TABLE `units`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
@@ -346,19 +226,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `users_types`
 --
 ALTER TABLE `users_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `votes`
---
-ALTER TABLE `votes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `words`
---
-ALTER TABLE `words`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Contraintes pour les tables déchargées
@@ -385,36 +253,11 @@ ALTER TABLE `suggestions`
   ADD CONSTRAINT `suggestions_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
 
 --
--- Contraintes pour la table `tracings`
---
-ALTER TABLE `tracings`
-  ADD CONSTRAINT `tracings_ibfk_1` FOREIGN KEY (`fk_word`) REFERENCES `words` (`id`);
-
---
--- Contraintes pour la table `units`
---
-ALTER TABLE `units`
-  ADD CONSTRAINT `units_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`);
-
---
 -- Contraintes pour la table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`fk_profile_picture`) REFERENCES `drawings` (`id`),
   ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`fk_user_type`) REFERENCES `users_types` (`id`);
-
---
--- Contraintes pour la table `votes`
---
-ALTER TABLE `votes`
-  ADD CONSTRAINT `votes_ibfk_1` FOREIGN KEY (`fk_user`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `votes_ibfk_2` FOREIGN KEY (`fk_drawing`) REFERENCES `drawings` (`id`);
-
---
--- Contraintes pour la table `words`
---
-ALTER TABLE `words`
-  ADD CONSTRAINT `words_ibfk_1` FOREIGN KEY (`fk_unit`) REFERENCES `units` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
