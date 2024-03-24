61605f9617ffa9981783c8bc4f1dd061a2bdcccb
Yannis Burkhalter <yannis@burkhalter.dev>
2024-03-24 15:42:27 +0100

    Merge CSSs

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -5,11 +5,8 @@
 		<title><?=$title?></title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width">
-		<meta name="description" content="<?=$this->lang->line('meta_description')?>" />
-		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/styles.css')?>">
-		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/menu.css')?>">
-		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/drawing.css')?>">
-		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/gallery.css')?>">
-		<link rel="icon" type="image/png" href="<?=base_url('assets/images/favicon.png')?>" />
+		<meta name="description" content="<?=$this->lang->line('meta_description')?>"/>
+		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/styles.css')?>"/>
+		<link rel="icon" type="image/png" href="<?=base_url('assets/images/favicon.png')?>"/>
 	</head>
 <body>
