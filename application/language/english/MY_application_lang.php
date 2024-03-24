44c0bc9831d47c883bdb3b497bb03c585ed85928
Yannis Burkhalter <yannis@burkhalter.dev>
2024-03-24 20:51:55 +0100

    Specify current language in root html tag

--- application/language/english/MY_application_lang.php
+++ application/language/english/MY_application_lang.php
@@ -1,5 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
+$lang['iso_code']                       = 'en';
+
 // Common
 $lang['pseudo']							= 'Pseudo:';
 $lang['mode']							= 'Mode:';
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,10 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
 <!DOCTYPE html>
-<html>
+<html lang="<?=$this->lang->line('iso_code')?>">
 	<head>
 		<title><?=$title?></title>
 		<meta charset="utf-8">
-		<!--TODO: lang-->
 		<meta name="viewport" content="width=device-width">
 		<meta name="description" content="<?=$this->lang->line('meta_description')?>"/>
 		<link rel="stylesheet" type="text/css" href="<?=base_url('assets/css/styles.css')?>"/>
