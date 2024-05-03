54f4a85d042f7a5e2c3700b6b8ff43d193b59aa5
Yannis Burkhalter <yannis@burkhalter.dev>
2024-05-03 20:06:41 +0200

    Update /misc/contact

--- application/views/misc/contact.php
+++ application/views/misc/contact.php
@@ -1,4 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed'); ?>
 <article>
-	<a href="https://github.com/BurkhalterY/normalux.ch" target="_blank">https://github.com/BurkhalterY/normalux.ch</a>
+	<a href="https://github.com/BurkhalterY/normalux.ch" target="_blank">
+		<img src="<?=base_url('assets/images/forkme.svg')?>" alt="Fork me on GitHub" title="Fork me on GitHub" class="fork"/>
+	</a>
 </article>
