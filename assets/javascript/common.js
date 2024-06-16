51151d95a70586f67e997ef321a7243b3e4c57df
Yannis Burkhalter <yannis@burkhalter.dev>
2024-06-17 00:08:59 +0200

    (#12) Attempt to fix scrolling

--- assets/javascript/common.js
+++ assets/javascript/common.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-	if (!window.location.hash) {
-		let defaultAnchor = document.getElementById("start")
-		if (defaultAnchor) defaultAnchor.scrollIntoView()
-	}
-})
+if (!window.location.hash) {
+    let defaultAnchor = document.getElementById("start")
+    if (defaultAnchor) defaultAnchor.scrollIntoView()
+}
