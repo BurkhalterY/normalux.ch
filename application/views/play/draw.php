2ea2e4f85d2220b5e6fc288e8c440c3de5635359
Yannis Burkhalter <yannis@burkhalter.dev>
2025-11-01 21:45:58 +0100

    Fix jump on color selection on mobile #12

--- application/views/play/draw.php
+++ application/views/play/draw.php
@@ -8,19 +8,19 @@
 	<canvas id="canvas" width="400" height="400" <?=$mode==ROTATION_MODE?'class="rotation-model"':''?> <?=$mode==BLINDED_MODE?'style="display: none;"':''?>></canvas>
 
 	<ul id="colors">
-		<li><a href="#" id="color-0" onclick="setColor(0)" class="color active" data-color="black"><?=$this->lang->line('color_black')?></a></li>
-		<li><a href="#" id="color-1" onclick="setColor(1)" class="color" data-color="gray"><?=$this->lang->line('color_gray')?></a></li>
-		<li><a href="#" id="color-2" onclick="setColor(2)" class="color" data-color="white"><?=$this->lang->line('color_white')?></a></li>
-		<li><a href="#" id="color-3" onclick="setColor(3)" class="color" data-color="red"><?=$this->lang->line('color_red')?></a></li>
-		<li><a href="#" id="color-4" onclick="setColor(4)" class="color" data-color="brown"><?=$this->lang->line('color_brown')?></a></li>
-		<li><a href="#" id="color-5" onclick="setColor(5)" class="color" data-color="orange"><?=$this->lang->line('color_orange')?></a></li>
-		<li><a href="#" id="color-6" onclick="setColor(6)" class="color" data-color="yellow"><?=$this->lang->line('color_yellow')?></a></li>
-		<li><a href="#" id="color-7" onclick="setColor(7)" class="color" data-color="green"><?=$this->lang->line('color_green')?></a></li>
-		<li><a href="#" id="color-8" onclick="setColor(8)" class="color" data-color="cyan"><?=$this->lang->line('color_cyan')?></a></li>
-		<li><a href="#" id="color-9" onclick="setColor(9)" class="color" data-color="blue"><?=$this->lang->line('color_blue')?></a></li>
-		<li><a href="#" id="color-10" onclick="setColor(10)" class="color" data-color="indigo"><?=$this->lang->line('color_indigo')?></a></li>
-		<li><a href="#" id="color-11" onclick="setColor(11)" class="color" data-color="violet"><?=$this->lang->line('color_purple')?></a></li>
-		<li><a href="#" id="color-12" onclick="setColor(12)" class="color" data-color="pink"><?=$this->lang->line('color_pink')?></a></li>
+		<li><i id="color-0" onclick="setColor(0)" class="color active" data-color="black"><?=$this->lang->line('color_black')?></i></li>
+		<li><i id="color-1" onclick="setColor(1)" class="color" data-color="gray"><?=$this->lang->line('color_gray')?></i></li>
+		<li><i id="color-2" onclick="setColor(2)" class="color" data-color="white"><?=$this->lang->line('color_white')?></i></li>
+		<li><i id="color-3" onclick="setColor(3)" class="color" data-color="red"><?=$this->lang->line('color_red')?></i></li>
+		<li><i id="color-4" onclick="setColor(4)" class="color" data-color="brown"><?=$this->lang->line('color_brown')?></i></li>
+		<li><i id="color-5" onclick="setColor(5)" class="color" data-color="orange"><?=$this->lang->line('color_orange')?></i></li>
+		<li><i id="color-6" onclick="setColor(6)" class="color" data-color="yellow"><?=$this->lang->line('color_yellow')?></i></li>
+		<li><i id="color-7" onclick="setColor(7)" class="color" data-color="green"><?=$this->lang->line('color_green')?></i></li>
+		<li><i id="color-8" onclick="setColor(8)" class="color" data-color="cyan"><?=$this->lang->line('color_cyan')?></i></li>
+		<li><i id="color-9" onclick="setColor(9)" class="color" data-color="blue"><?=$this->lang->line('color_blue')?></i></li>
+		<li><i id="color-10" onclick="setColor(10)" class="color" data-color="indigo"><?=$this->lang->line('color_indigo')?></i></li>
+		<li><i id="color-11" onclick="setColor(11)" class="color" data-color="violet"><?=$this->lang->line('color_purple')?></i></li>
+		<li><i id="color-12" onclick="setColor(12)" class="color" data-color="pink"><?=$this->lang->line('color_pink')?></i></li>
 	</ul>
 
 	<!--<div style="display: flex; justify-content: center;">
