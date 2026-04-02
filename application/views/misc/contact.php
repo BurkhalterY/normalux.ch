2784f4ed89f6e0ffc87ee4dec070ec432739d22b
Yannis Burkhalter <yannis@burkhalter.dev>
2026-04-02 20:13:47 +0000

    fix

--- application/views/misc/contact.php
+++ application/views/misc/contact.php
@@ -4,7 +4,7 @@
 		<img src="<?=base_url('assets/images/forkme.svg')?>" alt="Fork me on GitHub" title="Fork me on GitHub" class="fork"/>
 	</a>
 	<div style="display: flex; flex-direction: column; justify-content: center; align-items: center; gap: 20px; height: 100%;">
-		<a href="https://leaderweb.ch" target="_blank"><img src="<?=base_url('assets/images/leaderweb.svg')?>" alt="Leader Web" title="Leader Web" style="max-width: 150px;"></a>
-		<a href="https://leaderweb.ch" target="_blank"><div style="font-weight: bold;"><?=$this->lang->line('leader_web_sponso')?></div></a>
+		<a href="https://leaderweb.ch" target="_blank"><img src="<?=base_url('assets/images/leaderweb.svg')?>" alt="Leader Web" title="Leader Web" style="width: 150px; max-width: 100%;"></a>
+		<a href="https://leaderweb.ch" target="_blank"><div style="font-weight: bold; text-align: center;"><?=$this->lang->line('leader_web_sponso')?></div></a>
 	</div>
 </article>
